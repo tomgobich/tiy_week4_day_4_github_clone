96261a859f9438d6d143ab5209d1b823c38715dc
Tom Gobich <fallofmars@gmail.com>
2016-10-21 09:35:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# GitHub Personal Profile Clone
+
+##### The Iron Yard - Week 4, Day 4
+
+...
+
+
+### Tech Used
+
+- HTML
+- CSS / SASS
+- JavaScript
+- Gulp
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var babel         = require('gulp-babel');
+var browserSync   = require('browser-sync').create();
+var runSequence   = require('run-sequence');
+var postcss       = require('gulp-postcss');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('autoprefixer');
+
+// --------------------------------------------------
+// Development Processes
+// --------------------------------------------------
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+gulp.task('sass', () => {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError)) // converts sass to css using gulp-sass
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('autoprefixer', () => {
+  return gulp.src('./css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('babel', () => {
+  return gulp.src('js/*.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('dist/js/'));
+});
+
+// --------------------------------------------------
+// Task Watcher
+// --------------------------------------------------
+
+gulp.task('watch', ['browserSync', 'sass'], () => {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  // reloads the browser whenever HTML or JS files change
+  gulp.watch('css/**/*.css', ['autoprefixer']);
+  gulp.watch('js/**/*.js', ['babel']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('dist/css/**/*.css', browserSync.reload);
+  gulp.watch('dist/js/**/*.js', browserSync.reload);
+});
+
+// --------------------------------------------------
+// Build Sequences
+// --------------------------------------------------
+
+gulp.task('default', (callback) => {
+  runSequence(['sass', 'autoprefixer', 'babel', 'browserSync', 'watch'],
+    callback
+  )
+});
+
+
+
+
+
+
